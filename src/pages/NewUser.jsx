ee5d88acb60faae76ffe98f9d7cf30f67c580924
Syed Bital Ali <131905040+BitalSyed@users.noreply.github.com>
2025-09-14 16:21:10 +0500

    Update NewUser.jsx

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -1,6 +1,7 @@
 import { API_URL, getCookie } from "@/components/cookieUtils";
 import React, { useState } from "react";
 import { FaCheck } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 const NewUser = () => {
@@ -26,19 +27,19 @@ const NewUser = () => {
         token: getCookie("skillrextech_auth"),
         firstName,
         lastName,
-        username: userName+"@befab", 
+        username: userName + "@befab",
         userId,
         email,
         passwordHash: password,
         role,
       }),
     })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.error) {
-        toast.error(data.error);
-      } else {
-        toast.success(data.message);
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          toast.error(data.error);
+        } else {
+          toast.success(data.message);
         }
       })
       .catch((error) => {
@@ -54,9 +55,36 @@ const NewUser = () => {
     setRole("member");
   };
 
+  const navigate = useNavigate();
+
   return (
-    <div className="w-[80%] p-5 flex flex-col gap-5">
-      <div className="flex bg-white flex-col gap-5 border-2 border-gray-300 p-5 rounded-md">
+    <div className="w-[100%] p-5 flex flex-col gap-5">
+      <button
+        onClick={() => navigate("/users")}
+        class="bg-white text-center w-48 rounded-2xl h-14 relative text-black text-xl font-semibold group ml-auto cursor-pointer shadow-lg"
+        type="button"
+      >
+        <div class="bg-[#732e26] rounded-xl h-12 w-1/4 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1024 1024"
+            height="25px"
+            width="25px"
+          >
+            <path
+              d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+              fill="#ffffff"
+            ></path>
+            <path
+              d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+              fill="#ffffff"
+            ></path>
+          </svg>
+        </div>
+        <p class="translate-x-2">Back</p>
+      </button>
+
+      <div className="w-[80%] flex bg-white flex-col gap-5 border-2 border-gray-300 p-5 rounded-md">
         {/* First Name */}
         <div className="flex flex-col gap-1">
           <h3 className="text-lg font-semibold text-liblack">First Name</h3>
@@ -83,35 +111,34 @@ const NewUser = () => {
 
         {/* User Name */}
         <div className="flex flex-col gap-1">
-  <h3 className="text-lg font-semibold text-liblack">User Name</h3>
-  <div className="relative w-full">
-    <input
-      type="text"
-      placeholder="User Name"
-      value={userName}
-      onChange={(e) => setUserName(e.target.value)}
-      className="border text-liblack border-gray-500 outline-none rounded-md p-2 w-full pr-16"
-    />
-    <span className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
-      @Befab
-    </span>
-  </div>
-</div>
+          <h3 className="text-lg font-semibold text-liblack">User Name</h3>
+          <div className="relative w-full">
+            <input
+              type="text"
+              placeholder="User Name"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              className="border text-liblack border-gray-500 outline-none rounded-md p-2 w-full pr-16"
+            />
+            <span className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
+              @Befab
+            </span>
+          </div>
+        </div>
 
         {/* User Name */}
         <div className="flex flex-col gap-1">
-  <h3 className="text-lg font-semibold text-liblack">User ID</h3>
-  <div className="relative w-full">
-    <input
-      type="text"
-      placeholder="User ID"
-      value={userId}
-      onChange={(e) => setUserId(e.target.value)}
-      className="border text-liblack border-gray-500 outline-none rounded-md p-2 w-full pr-16"
-    />
-  </div>
-</div>
-
+          <h3 className="text-lg font-semibold text-liblack">User ID</h3>
+          <div className="relative w-full">
+            <input
+              type="text"
+              placeholder="User ID"
+              value={userId}
+              onChange={(e) => setUserId(e.target.value)}
+              className="border text-liblack border-gray-500 outline-none rounded-md p-2 w-full pr-16"
+            />
+          </div>
+        </div>
 
         {/* Email */}
         <div className="flex flex-col gap-1">
