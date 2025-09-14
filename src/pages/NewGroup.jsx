117b1ccb7c0715aeedae63ba05d091ddfabc55aa
Syed Bital Ali <131905040+BitalSyed@users.noreply.github.com>
2025-09-14 16:19:43 +0500

    Update NewGroup.jsx

--- src/pages/NewGroup.jsx
+++ src/pages/NewGroup.jsx
@@ -57,8 +57,32 @@ const NewGroup = () => {
 };
 
   return (
-    <div className="w-[80%] p-5 flex flex-col gap-5">
-      <div className="flex bg-white flex-col gap-5 border-2 border-gray-300 p-5 rounded-md">
+    <div className="w-[100%] p-5 flex flex-col gap-5">
+      <button
+        onClick={() => navigate("/groups")}
+        class="bg-white text-center w-48 rounded-2xl h-14 relative text-black text-xl font-semibold group ml-auto cursor-pointer shadow-lg"
+        type="button"
+      >
+        <div class="bg-[#732e26] rounded-xl h-12 w-1/4 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1024 1024"
+            height="25px"
+            width="25px"
+          >
+            <path
+              d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+              fill="#ffffff"
+            ></path>
+            <path
+              d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+              fill="#ffffff"
+            ></path>
+          </svg>
+        </div>
+        <p class="translate-x-2">Back</p>
+      </button>
+      <div className="w-[80%] flex bg-white flex-col gap-5 border-2 border-gray-300 p-5 rounded-md">
         
         {/* Group Name */}
         <div className="flex flex-col gap-1">
